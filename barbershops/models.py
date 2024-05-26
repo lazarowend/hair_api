d49387229ec87125490cdaeecca3bf681c8dae27
lazarowend <lazaroclans1@gmail.com>
2024-05-26 02:52:46 -0300

    create model barbershop

--- barbershops/models.py
+++ barbershops/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.hashers import make_password, check_password
 from django.db import models
 
-# Create your models here.
+
+class Barbershop(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=250)
+    phone = models.CharField(11)
+    email = models.EmailField(max_length=150)
+    password = models.CharField(max_length=255)
+    image_profile = models.ImageField(upload_to='media/profile/', blank=True, null=True)
+    
+    
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+    def __str__(self):
+        return self.name
--- barbershops/views.py
+++ barbershops/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
 
-# Create your views here.
+
+
+class ListCreateBarbershopView(ListCreateAPIView):
+    ...
